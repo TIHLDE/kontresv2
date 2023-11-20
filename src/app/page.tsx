0b489246b0c7978ab05806b8df3278b827165065
Embret Olav Rasmussen Roås <eoroaas@stud.ntnu.no>
2023-11-20 19:12:58 +0100

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-export default function Home() {
+export default function Home({ className }) {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className={`flex gap-3 ${className}`}>
       <div>git version embret</div>
     </main>
   );

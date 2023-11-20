83fdcbec8bdf33919ffac79036af926171cd5e1d
ConradOsvik <conrad.tinius.osvik@gmail.com>
2023-11-21 00:41:33 +0100

    feat(tailwind): added utility function for creating a default color out of a palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,56 @@
 import type { Config } from 'tailwindcss'
+//@ts-expect-error no types
 import themeSwapper from 'tailwindcss-theme-swapper'
 
-const base = {
-
+interface ColorRange {
+	50: string
+	100: string
+	200: string
+	300: string
+	400: string
+	500: string
+	600: string
+	700: string
+	800: string
+	900: string
+	DEFAULT: string
 }
 
-const dark = {
+type Shades = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900
 
-}
+const getColorsWithDefault = (colors: Omit<ColorRange, 'DEFAULT'>, defaultShade: Shades = 500) => ({
+	DEFAULT: colors[defaultShade],
+	...colors
+})
+
+const base = {}
+
+const dark = {}
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-
-    },
-  },
-  plugins: [
-    themeSwapper({
-      themes: [
-        {
-          name: 'base',
-          selectors: [':root'],
-          theme: base,
-        },
-        {
-          name: 'dark',
-          selectors: ['.dark'],
-          theme: dark,
-        }
-      ]
-    })
-  ],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		themeSwapper({
+			themes: [
+				{
+					name: 'base',
+					selectors: [':root'],
+					theme: base
+				},
+				{
+					name: 'dark',
+					selectors: ['.dark'],
+					theme: dark
+				}
+			]
+		})
+	]
 }
 export default config

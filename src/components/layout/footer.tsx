e76b441cdea842fbb35901deabaeb90600651d49
Conrad Osvik <conradosvik@dhcp-10-22-25-24.wlan.ntnu.no>
2024-01-15 16:29:51 +0100

    feat: update nav and footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className="h-screen w-screen p-16">
+		<footer className="h-screen w-screen p-8">
 			<div className="rounded-md bg-accent w-full h-full"></div>
 		</footer>
 	)

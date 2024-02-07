bbe78ea868c0f5ff1600c4219413ae144799b110
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-02-08 00:25:09 +0100

    Fixed render waterfall

--- src/components/layout/user-area.tsx
+++ src/components/layout/user-area.tsx
@@ -15,32 +15,31 @@ interface UserAreaProps extends React.HTMLProps<HTMLDivElement> {
     image: string;
 }
 
-export const UserArea = ({ className, ...props }: UserAreaProps) => {
-    const { data, loading, error, signOut } = useUser();
+export const UserArea = ({ username, image, className, ...props }: UserAreaProps) => {
+    // const { data, loading, error, signOut } = useUser();
     const router = useRouter();
 
+    const signOut = () => {
+        signOutUser();
+        router.refresh();
+    }
     return (
         <div {...props} className={cn('flex items-center justify-center w-fit gap-1', className)}>
             <Popover>
                 <PopoverTrigger>
 
                         <Avatar>
-                            <AvatarImage src={data?.image} alt={"profilbilde"} />
+                            <AvatarImage src={image} alt={"profilbilde"} />
                             <AvatarFallback><UserRound className="text-foreground" /></AvatarFallback>
                         </Avatar>
                 </PopoverTrigger>
                 <PopoverContent>
-                    {
-                        !error ? <>
-                                            <h2 className="text-lg">Hei, { data?.first_name }!</h2>
-                    <div className="flex gap-3 mt-1">
-                        <Button variant={"destructive"} className="w-full" onClick={signOut}>Logg ut</Button>
-                        <Button className="w-full">Kule ting</Button>
-                    </div>
-                    </> : <p>Kunne ikke laste inn panelet</p>
-                    }
-
-                </PopoverContent>
+                    <h2 className="text-lg">Hei, { username }!</h2>
+                        <div className="flex gap-3 mt-1">
+                            <Button variant={"destructive"} className="w-full" onClick={signOut}>Logg ut</Button>
+                            <Button className="w-full">Kule ting</Button>
+                        </div>
+               </PopoverContent>
             </Popover>
             <ModeToggle />
         </div>

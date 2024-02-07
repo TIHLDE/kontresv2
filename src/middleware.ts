1138059f61be5a7c13971f7f82925a2a498fe471
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-02-07 15:12:05 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,5 +26,5 @@ export const config = {
     * - _next/image (image optimization files)
     * - favicon.ico (favicon file)
     */
-        '/((?!api|login|favicon.ico).*)|_next/static|_next/image',
+        '/((?!api|login|_next/static|_next/image|_next|favicon.ico).*)',
 }

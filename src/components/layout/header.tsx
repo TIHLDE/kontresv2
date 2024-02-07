d53b2461f952702c87120c915a8d59155e966f46
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-02-07 17:04:07 +0100

    improved header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,8 +3,10 @@ import { cn } from '@/utils/cn';
 import { userAtom } from '@/utils/userAtom';
 import { useAtom } from 'jotai';
 import Image from 'next/image'
-import _Link from 'next/link'
+import _Link, { LinkProps } from 'next/link'
 import { UserArea } from './user-area';
+import Link from 'next/link';
+import { ReactNode } from 'react';
 
 interface HeaderProps extends React.HTMLProps<HTMLHeadElement> {
 	userData: User;
@@ -14,18 +16,18 @@ export default async function Header({ userData, className, ...props }: HeaderPr
 	//const navItems = await fetch('')
 	return (
 		<header {...props} className={cn("p-4 min-h-[80px] backdrop-blur-sm top-0 sticky w-full bg-background/80 border-b border-border justify-start items-center flex z-50", className)}>
-			<_Link href="/" className="mr-16">
+			<HeaderLink href="/" className="mr-16">
 				<Image
 					src="/tihlde.svg"
 					alt="tihlde logo"
 					width={200}
 					height={40}
 				/>
-			</_Link>
+			</HeaderLink>
 			<nav className='w-full'>
-				<Link href="/kontoret">Reserver kontoret</Link>
-				<Link href="/soundbox">Reserver soundbox</Link>
-				<Link href="/reservasjoner">Reservasjoner</Link>
+				<HeaderLink href="/kontoret">Reserver kontoret</HeaderLink>
+				<HeaderLink href="/soundbox">Reserver soundbox</HeaderLink>
+				<HeaderLink href="/reservasjoner">Reservasjoner</HeaderLink>
 			</nav>
 
 
@@ -38,13 +40,14 @@ export default async function Header({ userData, className, ...props }: HeaderPr
 	)
 }
 
-function Link({ href, children }: { href: string; children: React.ReactNode }) {
+function HeaderLink({ href, className, children, ...props }: LinkProps & { children?: ReactNode; className?: string; }) {
 	return (
-		<_Link
+		<Link
 			className="p-2 font-medium hover:bg-primary/5 rounded-md transition-colors duration-150"
 			href={href}
+			{...props}
 		>
 			{children}
-		</_Link>
+		</Link>
 	)
 }
--- src/components/layout/user-area.tsx
+++ src/components/layout/user-area.tsx
@@ -3,6 +3,9 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 import { Button } from "../ui/button"
 import { cn } from "@/utils/cn";
 import { ModeToggle } from "../ui/theme-mode-toggler";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { getUserData } from "@/utils/apis/TEMP";
+import { cookies } from "next/headers";
 
 interface UserAreaProps extends React.HTMLProps<HTMLDivElement> {
     username: string;
@@ -10,14 +13,22 @@ interface UserAreaProps extends React.HTMLProps<HTMLDivElement> {
 }
 
 export const UserArea = ({ username, image, className, ...props }: UserAreaProps) => {
+    const userData = getUserData();
+
     return (
         <div {...props} className={cn('flex items-center justify-center w-fit gap-1', className)}>
-            <Button variant={"ghost"}>
-                <Avatar>
-                    <AvatarImage src={image} alt={"profilbilde"} />
-                    <AvatarFallback><UserRound className="text-foreground" /></AvatarFallback>
-                </Avatar>
-            </Button>
+            <Popover>
+                <PopoverTrigger>
+
+                        <Avatar>
+                            <AvatarImage src={image} alt={"profilbilde"} />
+                            <AvatarFallback><UserRound className="text-foreground" /></AvatarFallback>
+                        </Avatar>
+                </PopoverTrigger>
+                <PopoverContent>
+                    <h2>Hei, {  }</h2>
+                </PopoverContent>
+            </Popover>
             <ModeToggle />
         </div>
     )

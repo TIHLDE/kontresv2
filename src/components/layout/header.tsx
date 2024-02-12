b3475b24ba291979a6c22d66327db373c6d0c1d0
Embret Olav Rasmussen Roås <eoroaas@stud.ntnu.no>
2024-02-12 17:30:29 +0100

    added footer and fix thew logo :)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,52 +1,62 @@
-"use client"
+'use client';
 
 import { User } from '@/types/User';
+
+import Logo from '@/components/ui/logo';
+
 import { cn } from '@/utils/cn';
-import Image from 'next/image'
-import { LinkProps } from 'next/link'
+
 import { UserArea } from './user-area';
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 import { ReactNode } from 'react';
 
 interface HeaderProps extends React.HTMLProps<HTMLHeadElement> {
-	userData: User;
+    userData: User;
 }
 
 export default function Header({ userData, className, ...props }: HeaderProps) {
-	return (
-		<header {...props} className={cn("p-4 min-h-[80px] backdrop-blur-sm top-0 sticky w-full bg-background/80 border-b border-border justify-start items-center flex z-50", className)}>
-			<HeaderLink href="/" className="mr-16">
-				<Image
-					src="/tihlde.svg"
-					alt="tihlde logo"
-					width={200}
-					height={40}
-				/>
-			</HeaderLink>
-			<nav className='w-full'>
-				<HeaderLink href="/kontoret">Reserver kontoret</HeaderLink>
-				<HeaderLink href="/soundbox">Reserver soundbox</HeaderLink>
-				<HeaderLink href="/reservasjoner">Reservasjoner</HeaderLink>
-			</nav>
+    return (
+        <header
+            {...props}
+            className={cn(
+                'p-4 min-h-[80px] backdrop-blur-sm top-0 sticky w-full bg-background/80 border-b border-border justify-start items-center flex z-50',
+                className,
+            )}
+        >
+            <HeaderLink href="/" className="mr-16">
+                <Logo />
+            </HeaderLink>
+            <nav className="w-full">
+                <HeaderLink href="/kontoret">Reserver kontoret</HeaderLink>
+                <HeaderLink href="/soundbox">Reserver soundbox</HeaderLink>
+                <HeaderLink href="/reservasjoner">Reservasjoner</HeaderLink>
+            </nav>
 
-
-			{userData ? (
-				<UserArea username={userData?.user_id ?? ''} image={userData?.image ?? ''} />
-			) : (
-				<p>Hvordan ser du dette??</p>
-			)}
-		</header>
-	)
+            {userData ? (
+                <UserArea
+                    username={userData?.user_id ?? ''}
+                    image={userData?.image ?? ''}
+                />
+            ) : (
+                <p>Hvordan ser du dette??</p>
+            )}
+        </header>
+    );
 }
 
-function HeaderLink({ href, className, children, ...props }: LinkProps & { children?: ReactNode; className?: string; }) {
-	return (
-		<Link
-			className="p-2 font-medium hover:bg-primary/5 rounded-md transition-colors duration-150"
-			href={href}
-			{...props}
-		>
-			{children}
-		</Link>
-	)
+function HeaderLink({
+    href,
+    className,
+    children,
+    ...props
+}: LinkProps & { children?: ReactNode; className?: string }) {
+    return (
+        <Link
+            className="p-2 font-medium hover:bg-primary/5 rounded-md transition-colors duration-150"
+            href={href}
+            {...props}
+        >
+            {children}
+        </Link>
+    );
 }

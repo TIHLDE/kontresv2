b85cbd7dba079b30062656f76762e4353aa05282
Embret Olav Rasmussen Roås <eoroaas@stud.ntnu.no>
2024-02-17 03:04:23 +0100

    color in navbar

--- src/components/ui/header-link.tsx
+++ src/components/ui/header-link.tsx
@@ -10,7 +10,7 @@ export default async function HeaderLink({
 }: LinkProps & { children?: ReactNode; className?: string }) {
     return (
         <Link
-            className="p-2 font-medium text-sm text-zinc-500 dark:text-zinc-600 hover:bg-primary/5 hover:text-primary rounded-md transition-colors duration-150"
+            className="p-2 font-medium text-sm text-zinc-500 dark:text-zinc-300 hover:bg-primary/5 hover:text-primary rounded-md transition-colors duration-150"
             href={href}
             {...props}
         >

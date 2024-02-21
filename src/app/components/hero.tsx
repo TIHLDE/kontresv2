483780dbddf9d1b54482f1836e24c96430b01e2c
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-02-21 23:36:26 +0100

    added fade-in and movement

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,7 +1,9 @@
 import { Button } from '@/components/ui/button';
 
-import WelcomeTitle from "@/app/components/welcometitle";
 
+
+import styles from './welcometitle.module.css';
+import WelcomeTitle from '@/app/components/welcometitle';
 import { ArrowLeft, ArrowRight } from 'lucide-react';
 import Image from 'next/image';
 
@@ -23,19 +25,22 @@ export default function Hero() {
                 className="-right-48 border hover:right-0 hover:shadow-2xl hover:scale-105 transition-all duration-300 translate-x-1/2 absolute top-1/2 -translate-y-1/2 rounded-2xl shadow"
             />
             <WelcomeTitle />
-            <p className="text-lg md:text-xl mt-8 mb-12 text-center text-muted-foreground">
-                Her kan du booke kontoret, soundbox og utstyr, <br />
-                om du er medlem i TIHLDE.
-            </p>
-            <div className="flex items-center">
-                <Button className="m-2 p-8 text-lg group">
-                    <ArrowLeft className="mr-2 group-hover:-translate-x-1 transition-transform duration-150" />
-                    Reserver kontoret{' '}
-                </Button>
-                <Button className="m-2 p-8 text-lg group" variant="outline">
-                    Reserver soundbox
-                    <ArrowRight className="ml-2 group-hover:translate-x-1 transition-transform duration-150" />
-                </Button>
+
+            <div className={`flex flex-col ${styles.action}`}>
+                <p className="text-lg md:text-xl mt-8 mb-12 text-center text-muted-foreground">
+                    Her kan du booke kontoret, soundbox og utstyr, <br />
+                    om du er medlem i TIHLDE.
+                </p>
+                <div className="flex items-center">
+                    <Button className="m-2 p-8 text-lg group">
+                        <ArrowLeft className="mr-2 group-hover:-translate-x-1 transition-transform duration-150" />
+                        Reserver kontoret{' '}
+                    </Button>
+                    <Button className="m-2 p-8 text-lg group" variant="outline">
+                        Reserver soundbox
+                        <ArrowRight className="ml-2 group-hover:translate-x-1 transition-transform duration-150" />
+                    </Button>
+                </div>
             </div>
         </div>
     );

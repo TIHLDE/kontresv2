acd38145992854a796a67e060b3a614f26cfb78d
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-03-05 00:43:32 +0100

    Added suspense to reservation page

---
+++ src/app/reservasjon/[id]/components/ReservationMetaWrapper.tsx
@@ -0,0 +1,25 @@
+import { getReservation } from '@/utils/apis/reservations';
+import { BaseGroup } from '@/utils/apis/types';
+
+import ReservationMeta from './ReservationMeta';
+
+const ReservationMetaWrapper = async ({
+    params,
+}: {
+    params: { id: string };
+}) => {
+    const reservation = await getReservation(params.id);
+    const from = new Date(reservation.start_time);
+    const to = new Date(reservation.end_time);
+    return (
+        <ReservationMeta
+            from={from.toISOString()}
+            to={to.toISOString()}
+            group={reservation.group_detail as BaseGroup}
+            state={reservation.state}
+            user={reservation.author_detail}
+        />
+    );
+};
+
+export default ReservationMetaWrapper;

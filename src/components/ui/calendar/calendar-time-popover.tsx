271c34ecd6d91cbc59135a6e87f7a01c5b834f6e
Embret Olav Rasmussen Roås <eoroaas@stud.ntnu.no>
2024-04-08 19:25:23 +0200

    fixed so that the position of the time hover effect is fixed to cursor so that it doesnt dissapear on scroll

--- src/components/ui/calendar/calendar-time-popover.tsx
+++ src/components/ui/calendar/calendar-time-popover.tsx
@@ -1,21 +1,21 @@
 export default function CalendarTimePopover({
-  day,
-  position,
-  timeStart,
-  timeEnd,
+    day,
+    position,
+    timeStart,
+    timeEnd,
 }: {
-  day: string;
-  position: { x: number; y: number };
-  timeStart: string;
-  timeEnd: string | null;
+    day: string;
+    position: { x: number; y: number };
+    timeStart: string;
+    timeEnd: string | null;
 }) {
-  return (
-    <div
-      className="z-20 absolute pointer-events-none border shadow-md text-sm bg-background rounded-md px-4 py-2 -translate-x-1/2 -translate-y-full"
-      style={{ top: position.y, left: position.x }}
-    >
-      {day + " " + timeStart}
-      {timeEnd && " - " + timeEnd}
-    </div>
-  );
+    return (
+        <div
+            className="z-20 fixed pointer-events-none border shadow-md text-sm bg-background rounded-md px-4 py-2 -translate-x-1/2 -translate-y-full"
+            style={{ top: position.y, left: position.x }}
+        >
+            {day + ' ' + timeStart}
+            {timeEnd && ' - ' + timeEnd}
+        </div>
+    );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,60 +1,60 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
-export function leftPad(value: number | string, length: number, padChar = "0") {
-  return String(value).padStart(length, padChar);
+export function leftPad(value: number | string, length: number, padChar = '0') {
+    return String(value).padStart(length, padChar);
 }
 
 export const weekDays = [
-  "Mandag",
-  "Tirsdag",
-  "Onsdag",
-  "Torsdag",
-  "Fredag",
-  "Lørdag",
-  "Søndag",
+    'Mandag',
+    'Tirsdag',
+    'Onsdag',
+    'Torsdag',
+    'Fredag',
+    'Lørdag',
+    'Søndag',
 ];
 
-
 export interface RelativeMousePositionProps {
-  x: number;
-  y: number;
-  day: number;
-  hours: number;
-  minutes: number;
+    x: number;
+    y: number;
+    day: number;
+    hours: number;
+    minutes: number;
 }
 export function getDateAtMousePosition<RelativeMousePositionProps>(
-  e: React.MouseEvent<HTMLDivElement>,
+    e: React.MouseEvent<HTMLDivElement>,
 ) {
-  const yPos = e.pageY - e.currentTarget.offsetTop;
-  const xPos = e.pageX - e.currentTarget.offsetLeft;
-  const height = e.currentTarget.clientHeight;
-  const width = e.currentTarget.clientWidth;
-  const day = Math.floor((xPos / width) * 7);
-  const time = Math.floor((yPos / height) * 288);
-  return {
-    x: xPos,
-    y: yPos,
-    day,
-    hours: Math.floor(time / 12),
-    minutes: (time % 12) * 5,
-  };
+    console.log(e.nativeEvent);
+    const yPos = e.nativeEvent.clientY;
+    const xPos = e.nativeEvent.clientX;
+    const height = e.currentTarget.clientHeight;
+    const width = e.currentTarget.clientWidth;
+    const day = Math.floor((xPos / width) * 7);
+    const time = Math.floor((yPos / height) * 288);
+    return {
+        x: xPos,
+        y: yPos,
+        day,
+        hours: Math.floor(time / 12),
+        minutes: (time % 12) * 5,
+    };
 }
 
 export function compareMousePositions(
-  a: RelativeMousePositionProps | null,
-  b: RelativeMousePositionProps | null,
+    a: RelativeMousePositionProps | null,
+    b: RelativeMousePositionProps | null,
 ) {
-  if (!a || !b) return false;
-  return (
-    a.x === b.x &&
-    a.y === b.y &&
-    a.day === b.day &&
-    a.hours === b.hours &&
-    a.minutes === b.minutes
-  );
+    if (!a || !b) return false;
+    return (
+        a.x === b.x &&
+        a.y === b.y &&
+        a.day === b.day &&
+        a.hours === b.hours &&
+        a.minutes === b.minutes
+    );
 }

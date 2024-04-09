611e6494add6c80f660667d81a20756e581ef212
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-04-09 23:38:30 +0200

    Added data table

--- src/app/min-side/page.tsx
+++ src/app/min-side/page.tsx
@@ -1,11 +1,30 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+
+import { getReservations } from '@/utils/apis/reservations';
 import { getCurrentUserData } from '@/utils/apis/user';
 
-const Page = async () => {
-    const user = getCurrentUserData();
+import ReservationTable from '@/app/admin/components/ReservationTable';
 
+const Page = async () => {
+    const user = await getCurrentUserData();
+
+    // Fetch the user's reservations
+    const reservations = await getReservations();
     return (
-        <div className='max-w-page mx-auto flex flex-col place-content-center'
-    )
+        <div className="max-w-page mx-auto flex flex-col place-content-center">
+            <Card className="w-full">
+                <CardHeader>
+                    <CardTitle>Min side</CardTitle>
+                    <Separator />
+                </CardHeader>
+                <CardContent>
+                    <ReservationTable data={reservations} />
+                    {user.first_name + ' ' + user.last_name}
+                </CardContent>
+            </Card>
+        </div>
+    );
 };
 
 export default Page;

a286a5b005ee42f5291470ca42f89912ad2deec7
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-08-27 12:03:05 +0200

    Booking loading skeleton

--- src/utils/apis/fetch.ts
+++ src/utils/apis/fetch.ts
@@ -1,47 +1,49 @@
-"use server"
+'use server';
 
 import { ACCESS_TOKEN, TOKEN_HEADER_NAME } from '../../../constants';
-import { cookies } from "next/headers";
+import { cookies } from 'next/headers';
 
 interface IFetchProps {
-  url: string;
-  config?: RequestInit
+    url: string;
+    config?: RequestInit;
 }
 
 export interface ErrorType {
-  status?: number;
-  statusText?: string;
-  response?: Record<string, unknown>;
+    status?: number;
+    statusText?: string;
+    response?: Record<string, unknown>;
 }
 
 export const IFetch = <T extends unknown>({ url, config }: IFetchProps) => {
-  let headers: HeadersInit = {}
-  headers[TOKEN_HEADER_NAME] = cookies().get(ACCESS_TOKEN)?.value as string;
-  headers = {
-    ...headers,
-    ...config?.headers
-  }
+    let headers: HeadersInit = {};
+    headers[TOKEN_HEADER_NAME] = cookies().get(ACCESS_TOKEN)?.value as string;
+    headers = {
+        ...headers,
+        ...config?.headers,
+    };
 
+    return fetch(url, {
+        ...config,
+        headers,
+    })
+        .then(async (res) => {
+            if (!res.ok) {
+                if (res.status === 500) {
+                    throw new Error('Internal server error');
+                }
 
-  return fetch(url, {
-    ...config,
-    headers
-  })
-    .then(async res => {
-      if (!res.ok) {
-        if (res.status === 500) {
-          throw new Error('Internal server error');
-        }
+                const message = await res.json();
+                const errorObject: ErrorType = {
+                    status: res.status,
+                    statusText: res.statusText,
+                    response: message,
+                };
+                throw new Error(JSON.stringify(errorObject));
+            }
 
-        const message = await res.json();
-        const errorObject: ErrorType = {
-          status: res.status,
-          statusText: res.statusText,
-          response: message
-        }
-        throw new Error(JSON.stringify(errorObject))
-      }
-
-      return res.json();
-    }) as Promise<T>
+            return res.json();
+        })
+        .catch((err) => {
+            throw new Error(err);
+        }) as Promise<T>;
 };

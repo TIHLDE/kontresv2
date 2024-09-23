109cc8c84f751e26e3cd34d9fa0259a7c7c9ff9e
Idun Tokle Larsen <iduntoklelarsen@dhcp-10-22-115-82.wlan.ntnu.no>
2024-09-23 19:34:21 +0200

    create tRPC endpoints for fetching and posting FAQs

---
+++ src/server/api/routers/faq.ts
@@ -0,0 +1,52 @@
+import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
+import { PrismaClient } from '@prisma/client';
+import { group } from 'console';
+import { get } from 'http';
+import { z } from 'zod';
+
+const prisma = new PrismaClient();
+
+export const faqRouter = createTRPCRouter({
+
+    //get all FAQs
+    getAll: publicProcedure.query(async () => {
+        const faqs = await prisma.fAQ.findMany();
+    }),
+
+    //get FAQ by id
+    getById: publicProcedure
+    .input(z.object({
+        questionId: z.number(),
+    }))
+    .query(async ({input}) => {
+        const faq = await prisma.fAQ.findUnique({
+            where: {
+                questionId: input.questionId,
+            }
+        });
+
+        if (!faq) {
+            throw new Error("Question could not be found!")
+        }
+
+        return faq;
+    }),
+
+    //create new question
+    create: publicProcedure.input(z.object({
+        question: z.string(),
+        answer: z.string(),
+        group: z.string(),
+        author: z.string(),
+    })).mutation(async ({input}) => {
+            const newFAQ = await prisma.fAQ.create({
+                data: {
+                    question: input.question,
+                    answer: input.answer,
+                    group: input.group,
+                    author: input.author
+                },
+            });
+            return newFAQ;
+    }),
+});

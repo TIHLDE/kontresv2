9a0901a2ebf8193a54424f9a6a9d4cffef97862d
Idun Tokle Larsen <iduntoklelarsen@dhcp-10-22-115-82.wlan.ntnu.no>
2024-09-23 18:59:08 +0200

    faq files

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,11 @@
-export default async function page() {
+import CardGrid from "./components/cardgrid";
+
+export default async function Page() {
     return(
         <>
         <h1>FAQ</h1>
+        <CardGrid >
+        </CardGrid>
         </>
     )
 }

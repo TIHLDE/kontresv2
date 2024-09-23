1a5ebfa729f93c09878717cf05ebba6e0067a5d4
Stcwal <stwalmann@outlook.com>
2024-09-23 19:37:08 +0200

    Made faq card and started layout on faq page
    Co-authored-by: Frikk Balder <MindChirp@users.noreply.github.com>

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,38 @@
+import FaqCard from "./components/faq-card";
+
 export default async function page() {
+    const data = [
+        {
+            description: "Test",
+            title: "Test",
+            user: {
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Huskiesatrest.jpg/264px-Huskiesatrest.jpg"
+            }
+        },{
+            description: "AHdajsdahdajdada",
+            title: "Test",
+            user: {
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Huskiesatrest.jpg/264px-Huskiesatrest.jpg"
+            }
+        },{
+            description: "Test2",
+            title: "Test2",
+            user: {
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Huskiesatrest.jpg/264px-Huskiesatrest.jpg"
+            }
+        }
+    ]
+
     return(
-        <>
-        <h1>FAQ</h1>
-        </>
+        <div className="max-w-page mx-auto min-h-screen flex flex-col gap-5 w-fit">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl w-fit">FAQ</h1>
+            <div className="grid grid-cols-3 gap-5">
+                {
+                    data.map((object, index) => ( 
+                        <FaqCard description={object.description} title={object.title} userImage={object.user.image} key={index} />
+                     ))
+                }
+            </div>
+        </div>
     )
 }
---
+++ src/utils/apis/fetch.ts
@@ -0,0 +1,50 @@
+'use server';
+
+import { ACCESS_TOKEN, TOKEN_HEADER_NAME } from '../../../constants';
+import { cookies } from 'next/headers';
+
+interface IFetchProps {
+    url: string;
+    config?: RequestInit;
+}
+
+export interface ErrorType {
+    status?: number;
+    statusText?: string;
+    response?: Record<string, unknown>;
+}
+
+export const IFetch = <T extends unknown>({ url, config }: IFetchProps) => {
+    let headers: HeadersInit = {};
+    headers[TOKEN_HEADER_NAME] = cookies().get(ACCESS_TOKEN)?.value as string;
+    headers = {
+        ...headers,
+        ...config?.headers,
+    };
+
+    return fetch("https://api-dev.tihlde.org"+url, {
+        ...config,
+        headers,
+    })
+        .then(async (res) => {
+            if (!res.ok) {
+                if (res.status === 500) {
+                    throw new Error('Internal server error');
+                }
+
+                const message = await res.json();
+                const errorObject: ErrorType = {
+                    status: res.status,
+                    statusText: res.statusText,
+                    response: message,
+                };
+                throw new Error(JSON.stringify(errorObject));
+            }
+
+            return res.json();
+        })
+        .catch((err) => {
+            console.error(err);
+            throw new Error(err);
+        }) as Promise<T>;
+};

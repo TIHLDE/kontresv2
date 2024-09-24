280c3119f0119bd55d84748ebd326272f6ddc1bb
Idun Tokle Larsen <iduntoklelarsen@Iduns-MacBook-Air.local>
2024-09-24 18:34:38 +0200

    first attempt

--- src/app/faq/components/cardgrid.tsx
+++
@@ -1,8 +0,0 @@
-import { type FAQ } from "@prisma/client";
-
-export default async function CardGrid(props: {}) {
-    
-    return(
-        <h1>hello</h1>
-    )
-}
--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -11,7 +11,13 @@ export default async function page() {
             <div className="grid grid-cols-3 gap-5">
                 {
                     data.map((object, index) => ( 
-                        <FaqCard description={object.answer} title={object.question} userImage={"https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg"} key={index} />
+                        <FaqCard 
+                            description={object.answer} 
+                            title={object.question} 
+                            userImage={"https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg"} 
+                            key={index} 
+                            questionId={object.questionId}
+                        />
                      ))
                 }
             </div>

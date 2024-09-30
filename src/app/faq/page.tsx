dc90659c38439708bd91a4163cdad947f6794d96
Idun Tokle Larsen <iduntoklelarsen@dhcp-10-22-74-96.wlan.ntnu.no>
2024-09-30 18:59:38 +0200

    route and page for creating new faq

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,6 +1,9 @@
 import { api } from "@/trpc/server";
 import FaqCard from "./components/faq-card";
 import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
+
 
 export default async function page() {
     const data = await api.faq.getAll();
@@ -8,7 +11,12 @@ export default async function page() {
 
     return(
         <div className="max-w-page mx-auto min-h-screen flex flex-col gap-5 w-fit">
-            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl w-fit">FAQ</h1>
+            <div className="flex justify-between">
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl w-fit">FAQ</h1>
+                <Link href={"faq/create"}>
+                    <Button><Plus className="w-5 h-5 mr-1" />Opprett ny</Button>
+                </Link>
+            </div>
             <div className="grid md:grid-cols-4 grid-cols-1 gap-5">
                 {
                     data.map((object, index) => ( 

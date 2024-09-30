fc205000a5922d99ad9ed733d96a3364f866fb59
Idun Tokle Larsen <iduntoklelarsen@dhcp-10-22-74-96.wlan.ntnu.no>
2024-09-30 18:59:38 +0200

    route and page for creating new faq

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,15 +1,8 @@
-import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-} from '@/components/ui/pagination';
+import { Button } from '@/components/ui/button';
 
 import FaqCard from './components/faq-card';
 import { api } from '@/trpc/server';
+import { Plus } from 'lucide-react';
 import Link from 'next/link';
 
 export default async function page() {
@@ -17,10 +10,18 @@ export default async function page() {
 
     return (
         <div className="max-w-page mx-auto min-h-screen flex flex-col gap-5 w-fit">
-            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl w-fit">
-                FAQ
-            </h1>
-            <div className="grid grid-cols-3 gap-5">
+            <div className="flex justify-between">
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl w-fit">
+                    FAQ
+                </h1>
+                <Link href={'faq/create'}>
+                    <Button>
+                        <Plus className="w-5 h-5 mr-1" />
+                        Opprett ny
+                    </Button>
+                </Link>
+            </div>
+            <div className="grid md:grid-cols-4 grid-cols-1 gap-5">
                 {data.map((object, index) => (
                     <Link href={`faq/${object.questionId}`} key={index}>
                         <FaqCard
@@ -29,8 +30,6 @@ export default async function page() {
                             userImage={
                                 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg'
                             }
-                            key={index}
-                            questionId={object.questionId}
                         />
                     </Link>
                 ))}

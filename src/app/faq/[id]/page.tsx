c3dd7589a7f1dda47924975eca11909821e94e70
Stcwal <stwalmann@outlook.com>
2024-09-30 18:54:57 +0200

    Edited design of FAQ card when clicked

--- src/app/faq/[id]/page.tsx
+++ src/app/faq/[id]/page.tsx
@@ -1,5 +1,6 @@
-import { CardHeader, Card, CardContent, CardTitle } from "@/components/ui/card";
 import { api } from "@/trpc/server";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 type QuestionPageParams = {
     params: {id: string}
@@ -10,12 +11,18 @@ export default async function Page({params: { id }}: QuestionPageParams) {
     const data = await api.faq.getById({questionId: parseInt(id)});
     return (
         <div className="max-w-page mx-auto h-screen -mt-24 flex flex-col justify-center items-center">
-            <Card className="w-80">
+            <Card className="w-100">
                 <CardHeader>
-                    <CardTitle className="text-center">{data.question}</CardTitle>
+                    <CardTitle className="text-left">{data.question}</CardTitle>
                 </CardHeader>
                 <CardContent>
                     {data.answer}
+                </CardContent>
+                <CardContent className="text-sm text-muted-foreground">
+                    {data.group}
+                </CardContent>
+                <CardContent className="text-sm text-muted-foreground">
+                    {data.author}
                 </CardContent>
             </Card>
         </div>

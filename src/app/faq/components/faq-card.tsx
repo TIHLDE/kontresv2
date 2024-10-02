810d0d9323b851ed8137a36c678aefc27eac155b
Stcwal <stwalmann@outlook.com>
2024-10-02 09:44:54 +0200

    Changed layout for FAQ cards in gridview and single-card view
    Co-authored-by: Frikk Balder <MindChirp@users.noreply.github.com>

--- src/app/faq/components/faq-card.tsx
+++ src/app/faq/components/faq-card.tsx
@@ -1,25 +1,41 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { BookableItem } from "@prisma/client";
+import Link from "next/link"
 
 type FaqCardProps = {
     title: string;
     description: string;
-    userImage: string;
+    bookableItems: BookableItem[];
+    author: string;
+    group: string;
 }
 
-export default function FaqCard({ description, title, userImage}: FaqCardProps) {
+export default async function FaqCard({ description, title,bookableItems, author, group}: FaqCardProps) {
 
     return (
-        <Card className="h-full"> 
-            <CardHeader>
-                <CardTitle>{title}</CardTitle>
-                <CardDescription>{ description }</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Avatar >
-                    <AvatarImage src={userImage} />
-                </Avatar>
-            </CardContent>
-        </Card>   
-        )
+        <div className="h-full overflow-hidden">
+            <Card className="w-100 h-full">
+                <CardHeader>
+                    <CardTitle className="text-left">{title}</CardTitle>
+                </CardHeader>   
+                <CardContent>
+                    {description}
+                </CardContent>
+                <CardContent className="text-xs text-muted-foreground">
+                    {author}, {group}
+                </CardContent>
+                <CardContent>
+                {bookableItems.map((item, index) => (
+                    <Link key={index} href={`faq/${""}`}> 
+                    <Badge key={index} className="m-1 ">
+                            {item.name}
+                    </Badge>
+                    </Link>   
+                ))}
+                </CardContent>
+            </Card>
+        </div>
+    );
 }

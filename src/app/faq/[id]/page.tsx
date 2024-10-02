27f3e2dcf3cbea6410123f9e5224edf124dcb804
Stcwal <stwalmann@outlook.com>
2024-10-02 09:44:54 +0200

    Changed layout for FAQ cards in gridview and single-card view
    Co-authored-by: Frikk Balder <MindChirp@users.noreply.github.com>

--- src/app/faq/[id]/page.tsx
+++ src/app/faq/[id]/page.tsx
@@ -1,30 +1,40 @@
 import { api } from "@/trpc/server";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { badgeVariants } from "@/components/ui/badge"
+import Link from 'next/link';   
+
 
 type QuestionPageParams = {
-    params: {id: string}
-}
+    params: { id: string };
+};
 
-export default async function Page({params: { id }}: QuestionPageParams) {
+export default async function Page({ params: { id } }: QuestionPageParams) {
+    const data = await api.faq.getById({ questionId: parseInt(id) });
 
-    const data = await api.faq.getById({questionId: parseInt(id)});
+    const { answer: description, question: title, author, group, bookableItems } = data;
     return (
         <div className="max-w-page mx-auto h-screen -mt-24 flex flex-col justify-center items-center">
             <Card className="w-100">
                 <CardHeader>
-                    <CardTitle className="text-left">{data.question}</CardTitle>
+                    <CardTitle className="text-left">{title}</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    {data.answer}
+                    {description}
                 </CardContent>
-                <CardContent className="text-sm text-muted-foreground">
-                    {data.group}
+                <CardContent className="text-xs text-muted-foreground">
+                    {author}, {group}
                 </CardContent>
-                <CardContent className="text-sm text-muted-foreground">
-                    {data.author}
+                <CardContent>
+                {bookableItems.map((bookableItem, index) => (
+                    <Link href={`./${""}`}> <Badge key={index} className="m-1 ">
+                            {bookableItem.name}
+                        </Badge>
+                    </Link>                        
+                    ))}
                 </CardContent>
-            </Card>
+            </Card> 
         </div>
     );
 }
--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,6 +1,16 @@
 import { api } from "@/trpc/server";
 import FaqCard from "./components/faq-card";
 import Link from "next/link";
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/components/ui/pagination"
+
 
 export default async function page() {
     const data = await api.faq.getAll();
@@ -9,19 +19,39 @@ export default async function page() {
     return(
         <div className="max-w-page mx-auto min-h-screen flex flex-col gap-5 w-fit">
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl w-fit">FAQ</h1>
-            <div className="grid md:grid-cols-4 grid-cols-1 gap-5">
+            <div className="grid md:grid-cols-3 md:grid-rows-4 grid-cols-1 gap-5">
                 {
                     data.map((object, index) => ( 
                         <Link href={`faq/${object.questionId}`} key={index}>
                             <FaqCard 
                                 description={object.answer} 
                                 title={object.question} 
-                                userImage={"https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg"} 
+                                bookableItems={object.bookableItems}
+                                author={object.author}
+                                group = {object.group}
                             />
                         </Link>
                      ))
                 }
             </div>
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href={`faq/${""}`} />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href={`faq/${""}`}>1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href={`faq/${""}`} />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </div>
+
+
     )
 }

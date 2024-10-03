4e1b8572b0b4ea8b22b6ac620e348cc3e9a1260a
Idun Tokle Larsen <iduntoklelarsen@dhcp-10-22-115-190.wlan.ntnu.no>
2024-10-03 16:53:14 +0200

    added form for submitting new questions, not fully working yet

--- src/app/faq/create/page.tsx
+++ src/app/faq/create/page.tsx
@@ -1,5 +1,23 @@
-export default async function createFaq(){
+import { Card } from "@/components/ui/card"
+import { LoadingSpinner } from "@/components/ui/loadingspinner";
+import { Suspense } from "react";
+import CreateFaqForm, { FaqFormValueTypes } from "./components/CreateFaqForm";
+import { api } from "@/trpc/server";
+
+export default async function page(){
+
+    async function onSubmit(formData: FaqFormValueTypes){
+        await api.faq.create({author: "daniel", group: "kok", ...formData})
+    }
+
     return(
-        <></>
+        <div className="max-w-page mx-auto min-h-screen gap-5 justify-center">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl w-fit p-4">Opprett ny FAQ</h1>
+            <Card className="p-4 h-fit w-full max-w-2xl">
+                <Suspense fallback={<LoadingSpinner className="mx-auto" />}>
+                    <CreateFaqForm onSubmit={onSubmit}/>
+                </Suspense>
+            </Card>
+        </div>
     )
 }

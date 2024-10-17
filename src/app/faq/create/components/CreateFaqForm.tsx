69bcc209f2af2e3711f172a5ec36147c0845b48a
Idun Tokle Larsen <iduntoklelarsen@dhcp-10-22-113-118.wlan.ntnu.no>
2024-10-17 17:18:27 +0200

    add toast on error

--- src/app/faq/create/components/CreateFaqForm.tsx
+++ src/app/faq/create/components/CreateFaqForm.tsx
@@ -60,6 +60,12 @@ export default function createFaqForm() {
             });
         } catch (error) {
             console.error(error);
+            toast({
+                variant: 'destructive',
+                description: 'Noe gikk galt:(',
+                duration: 6000,
+            });
+            return;
         }
         toast({
             description: '🎉Innlegget er opprettet🎉',

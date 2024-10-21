70e9030c01793c442a925ec7ba8aff21d9cb819a
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-10-21 19:39:54 +0200

    add middleware with protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,35 @@
-export { auth as middleware } from '@/auth';
+import { auth } from '@/auth';
+import { NextResponse } from 'next/server';
+
+export const middleware = auth((req) => {
+    if (!req.auth) {
+        const url = new URL('/login', req.nextUrl.origin);
+        url.searchParams.set('redirect', req.nextUrl.pathname);
+
+        return NextResponse.redirect(url);
+    }
+
+    if (
+        req.auth.user.role !== 'ADMIN' &&
+        req.nextUrl.pathname.startsWith('/admin')
+    )
+        return NextResponse.redirect(new URL('/', req.nextUrl.origin));
+
+    return;
+});
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - trpc (tRPC routes)
+         * - login (login page)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - $ (root path)
+         */
+        '/((?!api|trpc|login|_next/static|_next/image|favicon.ico|$).*)',
+    ],
+};

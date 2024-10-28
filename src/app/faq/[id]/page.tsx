bd2c6fc43799b049e0ac55bf13eac0b3992076eb
Stcwal <stwalmann@outlook.com>
2024-10-28 19:41:42 +0100

    Small fixes

--- src/app/faq/[id]/page.tsx
+++ src/app/faq/[id]/page.tsx
@@ -1,7 +1,7 @@
+import { Badge } from '@/components/ui/badge';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
-import { Badge } from "@/components/ui/badge";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { api } from "@/trpc/server";
+import { api } from '@/trpc/server';
 import Link from 'next/link';
 
 type QuestionPageParams = {
@@ -10,30 +10,32 @@ type QuestionPageParams = {
 
 export default async function Page({ params: { id } }: QuestionPageParams) {
     const data = await api.faq.getById({
-        questionId: +id
-    })
+        questionId: +id,
+    });
 
     return (
         <div className="max-w-page mx-auto h-screen -mt-24 flex flex-col justify-center items-center">
             <Card className="w-100">
                 <CardHeader>
-                    <CardTitle className="text-left">{data?.question}</CardTitle>
+                    <CardTitle className="text-left">
+                        {data?.question}
+                    </CardTitle>
                 </CardHeader>
-                <CardContent>
-                    {data?.answer}
-                </CardContent>
+                <CardContent>{data?.answer}</CardContent>
                 <CardContent className="text-xs text-muted-foreground">
                     {data?.author}, {data?.group}
                 </CardContent>
                 <CardContent>
-                {data?.bookableItems.map((bookableItem, index) => (
-                    <Link href={`./${""}`}> <Badge key={index} className="m-1 ">
-                            {bookableItem.name}
-                        </Badge>
-                    </Link>                        
+                    {data?.bookableItems.map((bookableItem, index) => (
+                        <Link href={`./${''}`}>
+                            {' '}
+                            <Badge key={index} className="m-1 ">
+                                {bookableItem.name}
+                            </Badge>
+                        </Link>
                     ))}
                 </CardContent>
-            </Card> 
+            </Card>
         </div>
     );
 }
--- src/server/api/routers/faq.ts
+++ src/server/api/routers/faq.ts
@@ -1,5 +1,7 @@
+import { createTRPCRouter, memberProcedure } from '@/server/api/trpc';
+
 import BookableItems from '@/components/ui/bookable-items';
-import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
+
 import { PrismaClient } from '@prisma/client';
 import { group } from 'console';
 import { get } from 'http';
@@ -8,88 +10,94 @@ import { z } from 'zod';
 const prisma = new PrismaClient();
 
 export const faqRouter = createTRPCRouter({
-
     //get all FAQs
-    getAll: publicProcedure
-    .input(z.object({
-        limit: z.number().min(1).max(100).optional(),
-        cursor: z.number().nullish(),
-        direction: z.enum(['forward', 'backward']),
-        filter: z.string().optional(),
-    }))
-    .query(async (options) => {
-        const { input } = options;
-        const limit = input.limit ?? 50;
-        const {cursor} = input;
+    getAll: memberProcedure
+        .input(
+            z.object({
+                limit: z.number().min(1).max(100).optional(),
+                cursor: z.number().nullish(),
+                direction: z.enum(['forward', 'backward']),
+                filter: z.string().optional(),
+            }),
+        )
+        .query(async (options) => {
+            const { input } = options;
+            const limit = input.limit ?? 50;
+            const { cursor } = input;
 
+            const faqs = await prisma.fAQ.findMany({
+                take: limit + 1,
+                cursor: cursor ? { questionId: cursor } : undefined,
+                where: {
+                    question: {
+                        contains: input.filter,
+                    },
+                },
+                orderBy: {
+                    questionId: 'asc',
+                },
+                include: {
+                    bookableItems: true,
+                },
+            });
 
-        const faqs = await prisma.fAQ.findMany({
-            take: limit + 1,
-            cursor: cursor ? {questionId: cursor} : undefined,
-            where: {
-                question: {
-                    contains: input.filter
-                }
-            },
-            orderBy: {
-                questionId: "asc"
-            },
-            include: {
-                bookableItems: true,
-            } 
-        });
+            let nextCursor: typeof cursor | undefined = undefined;
+            if (faqs.length > limit) {
+                const nextItem = faqs.pop();
+                nextCursor = nextItem!.questionId;
+            }
 
-        let nextCursor: typeof cursor | undefined = undefined;
-        if (faqs.length > limit) {
-            const nextItem = faqs.pop();
-            nextCursor = nextItem!.questionId;
-        }
-
-        return {
-            faqs,
-            nextCursor,
-        }
-    }),
+            return {
+                faqs,
+                nextCursor,
+            };
+        }),
 
     //get FAQ by id
-    getById: publicProcedure
-    .input(z.object({
-        questionId: z.number(),
-    }))
-    .query(async ({input}) => {
-        const faq = await prisma.fAQ.findUnique({
-            where: {
-                questionId: input.questionId,
-            },
-            include: {
-                bookableItems: true,
+    getById: memberProcedure
+        .input(
+            z.object({
+                questionId: z.number(),
+            }),
+        )
+        .query(async ({ input }) => {
+            const faq = await prisma.fAQ.findUnique({
+                where: {
+                    questionId: input.questionId,
+                },
+                include: {
+                    bookableItems: true,
+                },
+            });
+
+            if (!faq) {
+                throw new Error('Question could not be found!');
             }
-        });
 
-        if (!faq) {
-            throw new Error("Question could not be found!")
-        }
-
-        return faq;
-    }),
+            return faq;
+        }),
 
     //create new question
-    create: publicProcedure.input(z.object({
-        question: z.string(),
-        answer: z.string(),
-        group: z.string(),
-        author: z.string(),
-        bookableItemId: z.number(),
-    })).mutation(async ({input}) => {
+    create: memberProcedure
+        .input(
+            z.object({
+                question: z.string(),
+                answer: z.string(),
+                group: z.string(),
+                author: z.string(),
+                bookableItemId: z.number(),
+            }),
+        )
+        .mutation(async ({ input }) => {
             const newFAQ = await prisma.fAQ.create({
                 data: {
                     question: input.question,
                     answer: input.answer,
                     group: input.group,
-                    
-                    author: input.author
+
+                    author: input.author,
                 },
             });
             return newFAQ;
-    }),
+        }),
 });

f3c558d0385966ed00208e9a61bad5e4de0e5014
Stcwal <stwalmann@outlook.com>
2024-11-11 17:36:12 +0100

    created add/edit faq button

--- src/app/faq/[id]/page.tsx
+++ src/app/faq/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
+import { auth } from '@/auth';
 import { api } from '@/trpc/server';
 import Link from 'next/link';
 
@@ -9,33 +11,39 @@ type QuestionPageParams = {
 };
 
 export default async function Page({ params: { id } }: QuestionPageParams) {
-    const data = await api.faq.getById({
-        questionId: +id,
-    });
+    const data = await api.faq.getById({ questionId: +id });
+    const session = await auth();
+    let isAdmin = session?.user.role === 'ADMIN';
 
     return (
-        <div className="max-w-page mx-auto h-screen -mt-24 flex flex-col justify-center items-center">
-            <Card className="w-100">
-                <CardHeader>
-                    <CardTitle className="text-left">
-                        {data?.question}
-                    </CardTitle>
-                </CardHeader>
-                <CardContent>{data?.answer}</CardContent>
-                <CardContent className="text-xs text-muted-foreground">
-                    {data?.author}, {data?.group}
-                </CardContent>
-                <CardContent>
-                    {data?.bookableItems.map((bookableItem, index) => (
-                        <Link href={`./${''}`}>
-                            {' '}
-                            <Badge key={index} className="m-1 ">
-                                {bookableItem.name}
-                            </Badge>
-                        </Link>
-                    ))}
-                </CardContent>
-            </Card>
+        <div>
+            <div className="max-w-page mx-auto h-screen -mt-24 flex flex-col justify-center items-center gap-5">
+                <Card className="w-100">
+                    <CardHeader>
+                        <CardTitle className="text-left">
+                            {data?.question}
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent>{data?.answer}</CardContent>
+                    <CardContent className="text-xs text-muted-foreground">
+                        {data?.author}, {data?.group}
+                    </CardContent>
+                    <CardContent>
+                        {data?.bookableItems.map((bookableItem, index) => (
+                            <Link href={`./${''}`}>
+                                <Badge key={index} className="m-1 ">
+                                    {bookableItem.name}
+                                </Badge>
+                            </Link>
+                        ))}
+                    </CardContent>
+                </Card>
+                {isAdmin && (
+                    <Link href={'./create'} className="w-fit">
+                        <Button>Rediger</Button>
+                    </Link>
+                )}
+            </div>
         </div>
     );
 }

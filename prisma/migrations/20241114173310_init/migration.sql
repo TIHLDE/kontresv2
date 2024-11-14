6e878ef2f4288e60d897a9f397b36c643c6b0f59
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2024-11-14 18:33:28 +0100

    fixed migrations

--- prisma/migrations/20241114173310_init/migration.sql
+++ prisma/migrations/20241114173310_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "ReservationState" AS ENUM ('PENDING', 'CONFIRMED', 'CANCELLED');
+CREATE TYPE "ReservationState" AS ENUM ('PENDING', 'APPROVED', 'REJECTED');
 
 -- CreateTable
 CREATE TABLE "BookableItem" (
@@ -14,20 +14,20 @@ CREATE TABLE "BookableItem" (
 
 -- CreateTable
 CREATE TABLE "Reservation" (
-    "resservation_id" SERIAL NOT NULL,
-    "author" TEXT NOT NULL,
+    "reservationId" SERIAL NOT NULL,
+    "authorId" TEXT NOT NULL,
     "bookableItemId" INTEGER NOT NULL,
     "startTime" TIMESTAMP(3) NOT NULL,
     "endTime" TIMESTAMP(3) NOT NULL,
-    "state" "ReservationState" NOT NULL DEFAULT 'PENDING',
+    "status" "ReservationState" NOT NULL DEFAULT 'PENDING',
     "description" TEXT NOT NULL,
     "acceptedRules" BOOLEAN NOT NULL,
-    "group" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
     "servesAlcohol" BOOLEAN NOT NULL,
-    "soberWatch" TEXT NOT NULL,
-    "approvedBy" TEXT NOT NULL,
+    "soberWatch" TEXT,
+    "approvedById" TEXT,
 
-    CONSTRAINT "Reservation_pkey" PRIMARY KEY ("resservation_id")
+    CONSTRAINT "Reservation_pkey" PRIMARY KEY ("reservationId")
 );
 
 -- CreateIndex

418963c112cfba722388bb85c40201d253d642c1
Idun Tokle Larsen <idun.larsen@yahoo.no>
2025-02-10 16:36:52 +0100

    getting started

--- src/app/booking/[itemId]/page.tsx
+++ src/app/booking/[itemId]/page.tsx
@@ -1,19 +1,22 @@
-export default async function Page() {
+import { Card, CardHeader } from "@/components/ui/card";
+import { api } from '@/trpc/server';
+
+
+type ItemPageParams ={
+    params: { id: string};
+}
+
+export default async function Page({ params: { id }}: ItemPageParams) {
+    console.log(id)
+    const itemData = await api.bookableItem.getById({itemId: parseInt(id)}); //problemer med denne: nan
+    console.log(itemData)
     return (
         <div className="container max-w-page">
-            <h1 className="text-3xl font-semibold">Booking</h1>
-            <p className="text-sm text-muted-foreground mb-5">
-                Bla gjennom det TIHLDE har å tilby av kontor og utstyr
-            </p>
-            <div className="grid grid-cols-4 gap-4">
-                <div>
-                    <img
-                        className="aspect-video object-cover"
-                        src="https://images.unsplash.com/photo-1737251043885-1fa62cb12933?q=80&w=2573&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt="cover image"
-                    />
-                </div>
-            </div>
+            <Card>
+                <CardHeader>
+                    {itemData.name}
+                </CardHeader>
+            </Card>
         </div>
     );
 }

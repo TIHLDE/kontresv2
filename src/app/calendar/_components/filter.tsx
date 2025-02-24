90926520363c676e7d29caae9e97be057b881c9b
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-02-24 19:20:28 +0100

    add filter component for calendar

---
+++ src/app/calendar/_components/filter.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+
+import { api } from '@/trpc/react';
+import { parseAsInteger, useQueryState } from 'nuqs';
+
+export default function Filter() {
+    const { data: bookableItems } = api.bookableItem.getAll.useQuery();
+
+    const [id, setId] = useQueryState('id', parseAsInteger);
+
+    return (
+        <Card className="h-fit w-full lg:w-96">
+            <CardHeader>
+                <CardTitle>Filter</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <RadioGroup
+                    defaultValue={id?.toString()}
+                    onValueChange={(v) => setId(Number(v))}
+                >
+                    {bookableItems?.map((item) => (
+                        <div
+                            key={item.itemId}
+                            className="flex items-center gap-2"
+                        >
+                            <RadioGroupItem
+                                value={item.itemId.toString()}
+                                id={item.itemId.toString()}
+                            />
+                            <Label htmlFor={item.itemId.toString()}>
+                                {item.name}
+                            </Label>
+                        </div>
+                    ))}
+                </RadioGroup>
+            </CardContent>
+        </Card>
+    );
+}
--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,33 +1,11 @@
-// import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { Card, CardHeader, CardTitle } from '@/components/ui/card';
-
 import Calendar from './_components/calendar';
+import Filter from './_components/filter';
 
 export default function CalendarPage() {
-    const events = [
-        {
-            id: 1,
-            title: 'Event 1',
-            start: new Date(new Date().setHours(12, 0, 0, 0)),
-            end: new Date(
-                new Date(new Date().setDate(new Date().getDate() + 1)).setHours(
-                    9,
-                    0,
-                    0,
-                    0,
-                ),
-            ),
-        },
-    ];
-
     return (
-        <main className="flex p-2 gap-2 h-[calc(100vh_-_80px)]">
-            <Card className="h-fit w-96">
-                <CardHeader>
-                    <CardTitle>Filter</CardTitle>
-                </CardHeader>
-            </Card>
-            <Calendar events={events} />
+        <main className="flex flex-col lg:flex-row p-2 gap-2 h-[calc(100vh_-_80px)]">
+            <Filter />
+            <Calendar />
         </main>
     );
 }

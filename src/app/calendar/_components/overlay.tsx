eb47a9b9bff670f6812b88d01b5750e59d23434f
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-02-24 19:31:10 +0100

    feat: add event linking and improve drag overlay styles

--- src/app/calendar/_components/overlay.tsx
+++ src/app/calendar/_components/overlay.tsx
@@ -1,7 +1,12 @@
-import { Event } from './calendar';
+'use client';
+
+import { type Event } from './calendar';
 import { useDrag } from './drag-context';
 import { cn } from '@/lib/utils';
-import { isSameDay, isWithinInterval } from 'date-fns';
+import { format, isSameDay, isWithinInterval } from 'date-fns';
+import { nb } from 'date-fns/locale';
+import Link from 'next/link';
+import { type HTMLAttributes } from 'react';
 
 const dateToPixels = (date: Date) => {
     const hours = date.getHours();
@@ -9,10 +14,19 @@ const dateToPixels = (date: Date) => {
     return hours * 80 + minutes * (80 / 60);
 };
 
-function DragBlock({ style }: { style: React.CSSProperties }) {
+interface DragBlockProps extends HTMLAttributes<HTMLDivElement> {
+    className?: string;
+    style: React.CSSProperties;
+}
+
+function DragBlock({ className, style, ...rest }: DragBlockProps) {
     return (
         <div
-            className="absolute w-full z-5 pointer-events-none bg-red-500/10"
+            {...rest}
+            className={cn(
+                'absolute w-full z-10 pointer-events-none bg-red-500/20',
+                className,
+            )}
             style={style}
         />
     );
@@ -36,6 +50,7 @@ export function DragOverlay({ day }: { day: Date }) {
     if (isStart && isEnd) {
         return (
             <DragBlock
+                className="rounded-lg"
                 style={{
                     top: dateToPixels(dragState.start),
                     height:
@@ -49,6 +64,7 @@ export function DragOverlay({ day }: { day: Date }) {
     if (isStart) {
         return (
             <DragBlock
+                className="rounded-t-lg"
                 style={{
                     top: dateToPixels(dragState.start),
                     bottom: 0,
@@ -60,6 +76,7 @@ export function DragOverlay({ day }: { day: Date }) {
     if (isEnd) {
         return (
             <DragBlock
+                className="rounded-b-lg"
                 style={{
                     top: 0,
                     height: dateToPixels(dragState.end),
@@ -81,27 +98,37 @@ export function DragOverlay({ day }: { day: Date }) {
 }
 
 function EventBlock({
+    className,
     style,
-    color,
+    event,
 }: {
+    className?: string;
     style: React.CSSProperties;
-    color?: string;
+    event: Event;
 }) {
     return (
-        <div
+        <Link
+            href={`/reservation/${event.id}`}
             className={cn(
-                'absolute w-full z-5 pointer-events-none bg-blue-500/10',
-                color,
+                'absolute w-full z-5 bg-background hover:bg-muted cursor-pointer duration-150 p-2 flex flex-col overflow-hidden',
+                className,
+                event.color,
             )}
             style={style}
-        />
+        >
+            <h3 className="m-0">{event.title}</h3>
+            <p className="capitalize">
+                {format(event.start, 'EEE, dd.MMM HH:mm', { locale: nb })}
+            </p>
+            <p className="capitalize">
+                {format(event.end, 'EEE, dd.MMM HH:mm', { locale: nb })}
+            </p>
+        </Link>
     );
 }
 
 export function EventsOverlay({ day, events }: { day: Date; events: Event[] }) {
     return events.map((event) => {
-        console.log(event);
-
         const isStart = isSameDay(day, event.start);
         const isEnd = isSameDay(day, event.end);
         const isWithin =
@@ -115,12 +142,13 @@ export function EventsOverlay({ day, events }: { day: Date; events: Event[] }) {
         if (isStart && isEnd) {
             return (
                 <EventBlock
+                    className="rounded-lg"
                     style={{
                         top: dateToPixels(event.start),
                         height:
                             dateToPixels(event.end) - dateToPixels(event.start),
                     }}
-                    color={event.color}
+                    event={event}
                     key={event.id}
                 />
             );
@@ -129,11 +157,12 @@ export function EventsOverlay({ day, events }: { day: Date; events: Event[] }) {
         if (isStart) {
             return (
                 <EventBlock
+                    className="rounded-t-lg"
                     style={{
                         top: dateToPixels(event.start),
                         bottom: 0,
                     }}
-                    color={event.color}
+                    event={event}
                     key={event.id}
                 />
             );
@@ -142,11 +171,12 @@ export function EventsOverlay({ day, events }: { day: Date; events: Event[] }) {
         if (isEnd) {
             return (
                 <EventBlock
+                    className="rounded-b-lg"
                     style={{
                         top: 0,
                         height: dateToPixels(event.end),
                     }}
-                    color={event.color}
+                    event={event}
                     key={event.id}
                 />
             );
@@ -159,7 +189,7 @@ export function EventsOverlay({ day, events }: { day: Date; events: Event[] }) {
                         top: 0,
                         bottom: 0,
                     }}
-                    color={event.color}
+                    event={event}
                     key={event.id}
                 />
             );

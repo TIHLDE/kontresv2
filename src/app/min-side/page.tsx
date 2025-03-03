fcc760d2fab4c3d1ac8dbe7cd82396233edd9215
Stcwal <stwalmann@outlook.com>
2025-03-03 17:44:51 +0100

    refactor(min-side): reservation-view client side component

--- src/app/min-side/page.tsx
+++ src/app/min-side/page.tsx
@@ -1,25 +1,9 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from '@/components/ui/table';
 
-import { DataTable } from '../admin/components/data-table';
-import MyPageSkeleton from './components/my-page-skeleton';
-import MyPageWrapper from './components/my-page-wrapper';
-import { reservationColumns } from './components/reservation-columns';
 import ReservationView from './components/reservation-view';
 import { auth } from '@/auth';
 import { api } from '@/trpc/server';
-import { Reservation } from '@prisma/client';
 import { redirect, useRouter } from 'next/navigation';
-import { Suspense } from 'react';
 
 const Page = async () => {
     const session = await auth();
